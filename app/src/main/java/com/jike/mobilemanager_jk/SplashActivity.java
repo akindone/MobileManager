e06ed453e255ba1a5c53a75a1a7fbebfbe4a55fa
akindone <akindone@163.com>
2016-03-25 19:12:30 +0800

    1增加了网络连接失败的异常处理 2下载文件时增加进度条 3splash界面取消actionbar 4splash界面保证有几秒钟的展示时间 5细节提升：用户下载新版本后如果选择不安装，则界面跳转到主页

--- app/src/main/java/com/jike/mobilemanager_jk/SplashActivity.java
+++ app/src/main/java/com/jike/mobilemanager_jk/SplashActivity.java
@@ -12,7 +12,10 @@ import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.jike.utils.HttpUtils;
@@ -32,9 +35,21 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class SplashActivity extends Activity {
-
-    private static final int MSG_OK = 0;
     private static final String TAG ="SplashActivity" ;
+    private  static final String PATH= "http://192.168.3.8/MobileManager/";
+
+    private static final int MSG_OK = 1;//TO TEST 非0？？
+    private static final int MSG_ERROR_NOTFOUND = -1;
+    private static final int MSG_ERROR_SERVER = -2;
+    private static final int MSG_ERROR_IO =-3 ;
+    private static final int MSG_ERROR_JSON=-4;
+    private static final int MSG_ERROR_FNFE =-5 ;
+    private static final int MSG_ERROR_NNFE =-6 ;
+    private static final int MSG_ERROR_HTTPUTILS = -7;
+    private static final int MSG_WAIT_TIMEOUT = -8;
+
+    private String appVersionName;
+
     Handler handler=new Handler(){
         @Override
         public void handleMessage(Message msg) {
@@ -42,40 +57,66 @@ public class SplashActivity extends Activity {
             switch (msg.what){
                 case MSG_OK:
                     String[] strings = (String[]) msg.obj;
-                    Log.e(TAG,"handleMessage");
-
+                    Log.e(TAG, "handleMessage");
                     ifUpdate(strings);
+                    break;
+                case MSG_ERROR_NOTFOUND:
+                    Toast.makeText(SplashActivity.this, MSG_ERROR_NOTFOUND+"", Toast.LENGTH_SHORT).show();
+                    gotoHome();
+                    break;
+                case MSG_ERROR_SERVER:
+                    Toast.makeText(SplashActivity.this, MSG_ERROR_SERVER+"", Toast.LENGTH_SHORT).show();
+                    gotoHome();
+                    break;
+                case MSG_ERROR_IO:
+                    Toast.makeText(SplashActivity.this, MSG_ERROR_IO+"", Toast.LENGTH_SHORT).show();
+                    gotoHome();
+                    break;
+                case MSG_ERROR_JSON:
+                    Toast.makeText(SplashActivity.this, MSG_ERROR_JSON+"", Toast.LENGTH_SHORT).show();
+                    gotoHome();
+                    break;
+                case MSG_ERROR_FNFE:
+                    Toast.makeText(SplashActivity.this, MSG_ERROR_FNFE+"", Toast.LENGTH_SHORT).show();
+                    gotoHome();
+                    break;
+                case MSG_ERROR_NNFE:
+                    Toast.makeText(SplashActivity.this, MSG_ERROR_NNFE+"", Toast.LENGTH_SHORT).show();
+                    gotoHome();
+                    break;
+                case MSG_ERROR_HTTPUTILS:
+                    Toast.makeText(SplashActivity.this, MSG_ERROR_HTTPUTILS+"", Toast.LENGTH_SHORT).show();
+                    gotoHome();
+                    break;
+                case MSG_WAIT_TIMEOUT:
+                    //Toast.makeText(SplashActivity.this, MSG_WAIT_TIMEOUT+"", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(SplashActivity.this,Home.class));
+                    finish();
                     break;
             }
         }
     };
+    private ProgressBar pb_splash_download;
 
-    private String appVersionName;
-    private  static final String PATH= "http://192.168.28.1/MobileManager/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
-        Log.e(TAG,"onCreate");
-        new Thread(){
-            @Override
-            public void run() {
-                super.run();
-                try {
-                    Thread.currentThread().sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
+
         appVersionName = getAppVersionName();
+        TextView tv_splash_version = (TextView) findViewById(R.id.tv_splash_version);
+        pb_splash_download = (ProgressBar) findViewById(R.id.pb_splash_download);
+
+        tv_splash_version.setText(appVersionName);
+
         getLatestVersion();
     }
 
     private void ifUpdate(final String[] info) {
-       new AlertDialog.Builder(this)
+        Log.e(TAG, "ifUpdate");
+        new AlertDialog.Builder(SplashActivity.this)
                .setTitle("出新版本啦")
                .setMessage(info[1])
                .setPositiveButton("去更新", new DialogInterface.OnClickListener() {
@@ -89,21 +130,53 @@ public class SplashActivity extends Activity {
                .setNegativeButton("下次吧", new DialogInterface.OnClickListener() {
                    @Override
                    public void onClick(DialogInterface dialog, int which) {
-                       //TODO 跳转到主页
+                       gotoHome();
                    }
                })
                .show();
 
     }
 
+    private void waitaWhile(){
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                Message message = handler.obtainMessage();
+                try {
+                    Thread.currentThread().sleep(5000);
+                    message.what=MSG_WAIT_TIMEOUT;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+
+                } finally {
+                    handler.sendMessage(message);
+                }
+            }
+        }.start();
+    }
+
+    private void gotoHome() {
+        waitaWhile();
+    }
+
     private void download(String path) {
         AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
         asyncHttpClient.get(path, new AsyncHttpResponseHandler() {
             @Override
+            public void onProgress(long bytesWritten, long totalSize) {
+                super.onProgress(bytesWritten, totalSize);
+                pb_splash_download.setVisibility(View.VISIBLE);
+                pb_splash_download.setMax((int) totalSize);
+                pb_splash_download.setProgress((int)bytesWritten);
+            }
+
+            @Override
             public void onSuccess(int i, Header[] headers, byte[] bytes) {
+                Message message = handler.obtainMessage();
                 //把bytes数据保存到sd卡
                 File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/lastestMobileManager.apk");
-                Log.e(TAG,"download FILEPATH:"+file.getAbsolutePath());
+                Log.e(TAG, "download FILEPATH:" + file.getAbsolutePath());
                 FileOutputStream fos = null;
                 try {
                     fos = new FileOutputStream(file);
@@ -113,16 +186,20 @@ public class SplashActivity extends Activity {
 
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
+                    message.what=MSG_ERROR_FNFE;
                 } catch (IOException e) {
                     e.printStackTrace();
+                    message.what=MSG_ERROR_IO;
                 } finally {
                     if (fos != null) {
                         try {
                             fos.close();
                         } catch (IOException e) {
                             e.printStackTrace();
+                            message.what=MSG_ERROR_IO;
                         }
                     }
+                    handler.sendMessage(message);
                 }
             }
 
@@ -139,9 +216,15 @@ public class SplashActivity extends Activity {
         intent.setAction("android.intent.action.VIEW");
         intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
         Log.e(TAG,"install");
-        startActivity(intent);
+        startActivityForResult(intent, 100);//如果用户下载后不安装，要让界面可以跳转到主页，则要回调onActivityResult（）
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==100)
+            gotoHome();
+    }
 
     private void getLatestVersion() {
         Log.e(TAG,"getLatestVersion");
@@ -151,6 +234,7 @@ public class SplashActivity extends Activity {
             public void run() {
                 super.run();
                 String path=PATH+"latestVersion.json";
+                Message message = handler.obtainMessage();
                 try {
                     URL url = new URL(path);
                     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -159,50 +243,75 @@ public class SplashActivity extends Activity {
                     conn.setReadTimeout(5000);
                     conn.connect();
                     int responseCode = conn.getResponseCode();
-                    if (responseCode==200){
+                    Log.e(TAG,"responseCode"+responseCode);
+
+                    if (responseCode/100==4){
+                        message.what=MSG_ERROR_NOTFOUND;
+                    } else if (responseCode==500){
+                        message.what=MSG_ERROR_SERVER;
+                    } else if (responseCode==200){
                         Log.e(TAG,"getLatestVersion SCUCESS");
                         InputStream inputStream = conn.getInputStream();
 
                         //工具类 把流转为字符串
                         String data= HttpUtils.getStringFromStream(inputStream);
+                        if (data.equals("")){
+                            message.what=MSG_ERROR_HTTPUTILS;
+                        }else{
+                            //json解析
+                            JSONObject jsonObject = new JSONObject(data);
+                            String lastestVersionName = jsonObject.getString("versionName");
+                            String discription = jsonObject.getString("discription");
+                            String urlpath = jsonObject.getString("path");
+                            String[] msgs={lastestVersionName,discription,urlpath};
+                            Log.e(TAG,"getLatestVersion json:"+lastestVersionName+","+discription+","+urlpath);
 
-                        //json解析
-                        JSONObject jsonObject = new JSONObject(data);
-                        String lastestVersionName = jsonObject.getString("versionName");
-                        String discription = jsonObject.getString("discription");
-                        String urlpath = jsonObject.getString("path");
-                        String[] msgs={lastestVersionName,discription,urlpath};
-                        Log.e(TAG,"getLatestVersion json:"+lastestVersionName+","+discription+","+urlpath);
+                            float version = Float.parseFloat(appVersionName);
+                            float latestVer = Float.parseFloat(lastestVersionName);
+                            Log.e(TAG,"getLatestVersion:"+(latestVer>version));
 
-                        float version = Float.parseFloat(appVersionName);
-                        float latestVer = Float.parseFloat(lastestVersionName);
-                        Log.e(TAG,"getLatestVersion:"+(latestVer>version));
-
-                        if (latestVer>version){
-                            Message message = handler.obtainMessage(MSG_OK, msgs);
-                            handler.sendMessage(message);
-                        } else {
-                            //TODO 进入主页
+                            if (latestVer>version){
+                                message.what=MSG_OK;
+                                message.obj=msgs;
+                            } else {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        gotoHome();
+                                    }
+                                });
+                            }
                         }
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
+                    message.what=MSG_ERROR_IO;
+                    Log.e(TAG,"getLatestVersion IOException");
                 } catch (JSONException e) {
                     e.printStackTrace();
+                    message.what=MSG_ERROR_JSON;
+                    Log.e(TAG,"getLatestVersion JSONException");
+                } finally {
+                    handler.sendMessage(message);
                 }
             }
         }.start();
     }
 
+
     public String getAppVersionName() {
         Log.e(TAG,"getAppVersionName");
         String appVersionName="";
         PackageManager packageManager = getPackageManager();
+        Message message = handler.obtainMessage();
         try {
             PackageInfo packageInfo = packageManager.getPackageInfo(getPackageName(), 0);
             appVersionName = packageInfo.versionName;
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
+            message.what=MSG_ERROR_NNFE;
+        } finally {
+            handler.sendMessage(message);
         }
         return appVersionName;
     }
